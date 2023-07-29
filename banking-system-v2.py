49897e34e1ecdbf192ffa21a10a3f6a2841967b6
Jenifer Gonçalves da Silva <49965702+jeniferGoncalvesDaSilvaDev@users.noreply.github.com>
2023-07-28 22:43:10 -0300

    Add files via upload

--- banking-system-v2.py
+++ banking-system-v2.py
@@ -1,84 +1,109 @@
-import random 
+import random as r
 usuarios_nomes= []
 contas_correntes =[]
+saldo=0
+numeros_saques=0
+nome=str(input("insira seu nome: "))
+sobrenome=str(input("insira o seu sobrenome: "))
+idade=int(input("insira sua idade:"))
+cep=int(input("insira seu cep: "))
+rua=str(input("insira o endereço: "))
+numero_casa=int(input("insira o número da casa: "))
+cidade=str(input("insira a cidade: "))
+estado=str(input("insira seu estado: "))
+print("conta criada com sucesso! :)")
+conta=r.randint(0,1000)  
+print(f'sua conta corrente: {conta}')     
+      
+        
+        
+        
+            
+            
 
-def criar_conta():
-    while True:
-        usuario_nome=str(input("insira seu nome e sobrenome: "))
-        usuario_idade=int(input("insira sua idade: "))
-        usuario_sexo=str(input("insira seu sexo: m/f/não-binário "))
-        usuario_cidade=str(input("insira sua cidade: "))
-        usuario_cep=int(input("insira seu cep: "))
-        usuario_rua=str(input("insira seu endereço: "))
-        usuario_numero_casa=int(input("insira o número de sua casa: "))
-        usuario_complemento=str(input("complemento "))
-        usuario_estado=str(input("seu estado: "))
-        continuar= str(input("deseja continuar ? s/n  "))
-        #inserindo as variáveis em uma lista
-        usuarios_nomes.append(usuario_nome)
-        
-        
-        if continuar== "s":
-            print("continuando...")
-        elif continuar=="n":
-            print("obrigado")
-            #print(usuarios)
-            break
-        else:
-            print("comando inválido ")
-            
-            
-            
-def conta_corrente():
-        numero_conta=random.randint(0,1000)
-        for item in range(0,len(usuarios_nomes)):
-            print(f'seu nome {usuarios_nomes[item]} e o número de sua conta #000{numero_conta}')
-            contas_correntes.append(numero_conta)
-            
-        
                       
-dados={
-    "usuarios":usuarios_nomes,
-    "conta_corrente":contas_correntes
-}
 def bem_vindo():
     print("*"*100)
-    print("bem vindo ao cyber bank")
+    print(f"bem vindo ao cyber bank, {nome} {sobrenome}")
     print("para operaçoes, usa: d para deposito, e para extrato, s para saque, q para sair")
     opcao=input('insira o comando: ')
-    if opcao=="d":
-        print("deposito")
+    while True:
+        if opcao=="d":
+           print("deposito")
+           deposito()
+           
+        elif opcao=="s":
+            print("saque")
+            saque()
     
-#def saldo():
     
 def deposito():
-    while True:
      
       deposito = float(input('insira o valor:'))
-      saldo=0
+      global saldo
       print(f'seu saldo inicial: R$ {saldo:.2f}\n')
       saldo+=deposito
       print(f'seu saldo atual: R$ {saldo:.2f}\n')
-      continuar=str(input("quer continuar? s/n: ") 
-      if continuar=="n":
-          print(f'seu saldo atual: R$ {saldo:.2f}\n')
-          break 
+      if deposito < 0:
+            print("saldo negativo")
+            modulo= -1
+            saldo_corrigido= saldo * modulo
+            saldo=saldo_corrigido
+            print(f'seu saldo corrigido: R$ {saldo:.2f}\n')
+            
+         
+def saque():
+          saque = float(input('insira o valor: '))
+          global saldo 
+          print(f'valor inicial do saldo: R$ {saldo:.2f}\n')
+          saldo-=saque
+          print(f'valor sacado: R$ {saque:.2f}\n')
+          print(f'valor atual do saldo: R$ {saldo:.2f}\n')
           
+              
+         
+  
           
-       
-          
-      
-    
-     
-#def extrato():
-      
 #chamando as funções e imprimindo variáveis 
+
+
+bem_vindo()
+
+
+
+
+aques in range(1,4):
+            if numeros_saques >=3:
+               print('limite diário de saques atingidos! ')
+               print(f'valor atual do saldo: R$ {saldo:.2f}\n')
+               break 
+              
+         
+  
+          
+#chamando as funções e imprimindo variáveis 
+
+
+bem_vindo()
+
+
+
+
+()
+
+
+
+
+indo variáveis 
 #criar_conta()
 #conta_corrente()
 bem_vindo()
-deposito()
+#deposito()
 #print(usuarios_nomes)
 #print(contas_correntes)
 #print(dados)
+
+
+print(dados)
 
 
